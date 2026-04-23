7b05bd65dd93dd923388bd74472f3970fc3fe20e
Krish Sapru <ksapru@bu.edu>
2026-04-23 16:35:38 -0400

    Average metrics

--- models/metrics/average_metrics.sql
+++ models/metrics/average_metrics.sql
@@ -1,2 +1,10 @@
-SELECT * FROM `krish-dev-data.ads_raw.cleaned_util`
-LIMIT 10
+CREATE OR REPLACE TABLE `krish-dev-data.ads_raw.average_metrics` AS 
+SELECT 
+    new_ts,
+    COUNT(DISTINCT machine_id) as unique_machines,
+    AVG(CPU) as avg_cpu,
+    AVG(memory) as avg_memory,
+    priority
+FROM `krish-dev-data.ads_raw.cleaned_util`
+GROUP BY new_ts,priority
+ORDER BY new_ts ASC
