89f6b9abd8aec0f24798f829a4b383204a19de5f
Krish Sapru <ksapru@bu.edu>
2026-04-28 18:16:22 -0400

    remove deps

--- target/compiled/models/metrics/average_metrics.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    new_ts,
-    COUNT(DISTINCT machine_id) as unique_machines,
-    AVG(CPU) as avg_cpu,
-    AVG(memory) as avg_memory
-FROM `krish-dev-data`.`ads_dev`.`cleaned_util`
-GROUP BY 1
--- target/compiled/models/staging/cleaned_util.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  TIMESTAMP_TRUNC(ts, MINUTE) AS new_ts,
-  machine_id,
-  cpu,
-  memory,
-  priority
-FROM `krish-dev-data`.`ads_dev`.`initial_raw_dataset`
-WHERE ts IS NOT NULL
